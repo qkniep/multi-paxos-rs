614ccf961b7558c69afb597d9027346574a1c107
Quentin Kniep <hello@quentinkniep.com>
2020-07-17 17:06:01 +0000

    fix failing build

--- src/bin/paxos_replica.rs
+++ src/bin/paxos_replica.rs
@@ -1,6 +1,10 @@
 // Copyright (C) 2020 Quentin M. Kniep <hello@quentinkniep.com>
 // Distributed under terms of the MIT license.
 
-fn main() {
-    start_replica(0, 6);
+use std::io;
+
+fn main() -> io::Result<()> {
+    paxos::start_replica(0, 6)?;
+
+    Ok(())
 }
