84d227c2df46ddce630f90c77f5739614323d01c
Quentin Kniep <hello@quentinkniep.com>
2020-10-28 09:19:59 +0100

    extend bank example

--- src/bin/bank/main.rs
+++ src/bin/bank/main.rs
@@ -7,7 +7,7 @@ use serde::{Deserialize, Serialize};
 use tracing::Level;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub enum ClientCommand {
+pub enum CustomerAction {
     Open {
         account: String,
     },
@@ -26,16 +26,6 @@ pub enum ClientCommand {
     },
 }
 
-/// Creates and connects a number of branch offices for the bank.
-pub fn setup_offices(office_count: usize) -> io::Result<()> {
-    // create various network nodes and start them
-    for _address in 0..office_count {
-        paxos::start_replica(office_count);
-    }
-
-    Ok(())
-}
-
 fn main() -> io::Result<()> {
     use tracing_subscriber::{fmt::time::ChronoLocal, FmtSubscriber};
 
@@ -45,14 +35,36 @@ fn main() -> io::Result<()> {
         .with_max_level(Level::TRACE)
         .init();
 
-    // create and connect a number of offices
-    let _channels = setup_offices(6)?;
-
-    // paxos::submit_value(&ClientCommand::Open {
-    //     account: "Peter".to_string(),
-    // });
-
+    // create and connect a number of bank branches
+    let branches = paxos::start_replicas(5);
     sleep(Duration::new(2, 0));
+
+    paxos::submit_value(branches[0], &CustomerAction::Open {
+        account: "Peter".to_string(),
+    });
+    paxos::submit_value(branches[3], &CustomerAction::Open {
+        account: "Dieter".to_string(),
+    });
+
+    paxos::submit_value(branches[1], &CustomerAction::Deposit {
+        account: "Peter".to_string(),
+        amount: 100,
+    });
+    paxos::submit_value(branches[2], &CustomerAction::Withdraw {
+        account: "Dieter".to_string(),
+        amount: 50,
+    });
+
+    paxos::submit_value(branches[4], &CustomerAction::Deposit {
+        account: "Dieter".to_string(),
+        amount: 60,
+    });
+    paxos::submit_value(branches[0], &CustomerAction::Withdraw {
+        account: "Peter".to_string(),
+        amount: 80,
+    });
+
+    sleep(Duration::new(3, 0));
 
     Ok(())
 }
